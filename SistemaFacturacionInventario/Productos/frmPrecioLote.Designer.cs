ec78457c3f5506d6665e0d29199ddd43cfeef878
cesar91bo <cesar91bo@gmail.com>
2025-05-21 20:56:57 -0300

    MD:Modicaciones de interfaz de cambio de precios por lotes

--- SistemaFacturacionInventario/Productos/frmPrecioLote.Designer.cs
+++ SistemaFacturacionInventario/Productos/frmPrecioLote.Designer.cs
@@ -36,7 +36,16 @@
             this.lblFiltrar = new System.Windows.Forms.Label();
             this.lblListaPrecios = new System.Windows.Forms.Label();
             this.listViewProductos = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPorcCambio = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnBuscar
@@ -97,9 +106,9 @@
             this.lblListaPrecios.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
             this.lblListaPrecios.Location = new System.Drawing.Point(309, 4);
             this.lblListaPrecios.Name = "lblListaPrecios";
-            this.lblListaPrecios.Size = new System.Drawing.Size(127, 21);
+            this.lblListaPrecios.Size = new System.Drawing.Size(141, 21);
             this.lblListaPrecios.TabIndex = 6;
-            this.lblListaPrecios.Text = "Lista de Precios";
+            this.lblListaPrecios.Text = "Buscar Productos";
             // 
             // listViewProductos
             // 
@@ -109,16 +118,105 @@
             this.listViewProductos.Location = new System.Drawing.Point(73, 114);
             this.listViewProductos.MultiSelect = false;
             this.listViewProductos.Name = "listViewProductos";
-            this.listViewProductos.Size = new System.Drawing.Size(1016, 184);
+            this.listViewProductos.Size = new System.Drawing.Size(1016, 230);
             this.listViewProductos.TabIndex = 32;
             this.listViewProductos.UseCompatibleStateImageBehavior = false;
             this.listViewProductos.View = System.Windows.Forms.View.Details;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(467, 362);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 21);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Productos Seleccionados";
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(73, 397);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1016, 230);
+            this.listView1.TabIndex = 35;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.label1.Location = new System.Drawing.Point(13, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Porcentaje de Cambio:";
+            // 
+            // txtPorcCambio
+            // 
+            this.txtPorcCambio.Location = new System.Drawing.Point(141, 23);
+            this.txtPorcCambio.Name = "txtPorcCambio";
+            this.txtPorcCambio.Size = new System.Drawing.Size(82, 22);
+            this.txtPorcCambio.TabIndex = 36;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(259, 24);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(75, 17);
+            this.radioButton1.TabIndex = 37;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Aumentar";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(359, 24);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 17);
+            this.radioButton2.TabIndex = 38;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Disminuir";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(497, 703);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 36);
+            this.button1.TabIndex = 39;
+            this.button1.Text = "Aplicar Cambios";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.txtPorcCambio);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(73, 633);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(520, 64);
+            this.groupBox1.TabIndex = 40;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Modificación de precios";
             // 
             // frmPrecioLote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1134, 597);
+            this.ClientSize = new System.Drawing.Size(1134, 751);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.listViewProductos);
@@ -127,7 +225,10 @@
             this.Load += new System.EventHandler(this.frmPrecioLote_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -140,5 +241,13 @@
         private System.Windows.Forms.Label lblFiltrar;
         private System.Windows.Forms.Label lblListaPrecios;
         private System.Windows.Forms.ListView listViewProductos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPorcCambio;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
